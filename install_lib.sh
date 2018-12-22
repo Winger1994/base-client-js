3af4c7d7658949930217d0f19623935808789667
Yulei <yuleil@andrew.cmu.edu>
2018-12-21 19:19:37 -0800

    update compiled data

--- install_lib.sh
+++ install_lib.sh
@@ -10,14 +10,13 @@ npm run build
 #cp ./dist/Base.js* ./widget
 #cp -rf ./dist/src/* example/public/base-lib/@types/base/
 
+cp ./src/repository/offer/Purchase.json ./dist/src/repository/offer/
 cp ./dist/Bitclave-Base.js* example/public/base-lib/bitclave-base/
 #cp ./dist/Bitclave-Base.js* ./widget
 cp -rf ./dist/src/* example/public/base-lib/@types/bitclave-base/
 mv ./example/public/base-lib/@types/bitclave-base/Base.d.ts ./example/public/base-lib/@types/bitclave-base/index.d.ts
 cd example/
-npm install
 
 cp -rf public/base-lib/* node_modules/
-npm run build
 node ../remove-bitcore-lib-dep.js
 popd
